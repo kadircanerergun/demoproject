3faacd9a9786c7e1735fdfc71f6fedf77077de49
Kadir Caner Ergün <caner.ergun@mynetgroup.com>
2019-02-24 22:25:41 +0300

    Add sort parameter

--- app/src/main/java/com/kce/trendyol/network/Services/PhotoRepository.java
+++ app/src/main/java/com/kce/trendyol/network/Services/PhotoRepository.java
@@ -1,5 +1,7 @@
 package com.kce.trendyol.network.Services;
 
+import android.support.annotation.Nullable;
+
 import com.kce.trendyol.models.ApiResponse;
 
 import retrofit2.Call;
@@ -10,13 +12,13 @@ public interface PhotoRepository {
 
 
     //https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=&format=json&per_page=10&extras=owner_name
-    @GET("/services/rest?extras=owner_name,icon_server&tags=world")
+    @GET("/services/rest?extras=owner_name,icon_server&tags=world&sort=date-posted-desc")
     Call<ApiResponse> fetchResult(
             @Query("method") String method,
             @Query("page") int page,
             @Query("per_page") int perPage
     );
-    @GET("/services/rest?extras=owner_name,icon_server")
+    @GET("/services/rest?extras=owner_name,icon_server&sort=date-posted-desc")
     Call<ApiResponse> fetchResultWithTag(
             @Query("method") String method,
             @Query("page") int page,
